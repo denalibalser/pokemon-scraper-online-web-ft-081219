42614c8b53dd30b4c54d2600d0408231ce21ea5b
Denali Balser <denali.balser167@gmail.com>
2019-09-26 00:31:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id
 
-  def initialize(id=nil, name, type)
+  def initialize(:id=nil, :name, :type)
     @id = id
     @name = name
     @type = type
