d1b877f6ec19bd9c234152ef1b6b5cc6919abbae
Denali Balser <denali.balser167@gmail.com>
2019-09-26 00:44:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,11 +10,9 @@ class Pokemon
   end
 
   def self.save
-    sql = <<-SQL
-      INSERT INTO pokemon(name, type)
-      VALUES (?, ?)
-      SQL
-    DB[:conn].execute(sql, self.name, self.type)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+
+    DB.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", [name, type])
+    
+
   end
 end
